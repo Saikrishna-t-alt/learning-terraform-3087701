859fe8f6b1520cbb0499f2ed21b1c1a85196a599
Saikrishna-t-alt <darkhunter123457@gmail.com>
2026-09-07 16:04:46 +0530

    modules

--- main.tf
+++ main.tf
@@ -19,48 +19,23 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "sk" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = [aws_security_group.sk.id]
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.sk_sg.security_group_id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
 
-resource "aws_security_group" "sk" {
-  name = "sk"
-  description = "Allow http and https inbound. allow all outbound"
+module "sk_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "sk_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.sk.id
-}
-
-
-resource "aws_security_group_rule" "sk_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.sk.id
-}
-
-
-resource "aws_security_group_rule" "sk_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.sk.id
+  vpc_id  = data.aws_vpc.default.id
+  name    = "sk"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
