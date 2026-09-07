7b2ada9d18cf82f57cf732ee25520047517046fe
Saikrishna-t-alt <darkhunter123457@gmail.com>
2026-09-07 17:15:09 +0530

    AS and alb

--- main.tf
+++ main.tf
@@ -31,17 +31,6 @@ module "sk_vpc" {
   }
 }
 
-resource "aws_instance" "sk" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  subnet_id              = module.sk_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.sk_sg.security_group_id]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
-
 module "sk_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
@@ -52,4 +41,53 @@ module "sk_sg" {
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "sk_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "sk"
+
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.sk_vpc.public_subnets
+  target_group_arns   = module.sk_alb.target_group_arns
+  security_groups     = [module.sk_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
+}
+
+module "sk_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "sk-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.sk_vpc.vpc_id
+  subnets            = module.sk_vpc.public_subnets
+  security_groups    = [module.sk_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "sk-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
 }
--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "instance_ami" {
-  value = aws_instance.sk.ami
-}
-
-output "instance_arn" {
-  value = aws_instance.sk.arn
-}
