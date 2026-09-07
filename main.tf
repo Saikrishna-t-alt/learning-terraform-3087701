f3be9246c1ccebcc178bb8f5cf6ac4c288bd95c2
Saikrishna-t-alt <darkhunter123457@gmail.com>
2026-09-07 16:21:52 +0530

    vpc

--- main.tf
+++ main.tf
@@ -14,13 +14,27 @@ data "aws_ami" "app_ami" {
   owners = ["amazon"]
 }
 
-data "aws_vpc" "default" {
-  default = true
+
+module "sk_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a","ap-south-1b","ap-south-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "sk" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
+  subnet_id              = module.sk_vpc.public_subnets[0]
   vpc_security_group_ids = [module.sk_sg.security_group_id]
 
   tags = {
@@ -32,7 +46,7 @@ module "sk_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.sk_vpc.vpc_id
   name    = "sk"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
