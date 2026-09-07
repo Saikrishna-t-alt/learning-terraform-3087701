fdbe145712ff8adf538c577918228dea694c6fa9
Saikrishna-t-alt <darkhunter123457@gmail.com>
2026-09-07 15:31:51 +0530

    SG

--- main.tf
+++ main.tf
@@ -14,11 +14,53 @@ data "aws_ami" "app_ami" {
   owners = ["amazon"]
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "sk" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [aws_security_group.sk.id]
+
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "sk" {
+  name = "sk"
+  description = "Allow http and https inbound. allow all outbound"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "sk_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sk.id
+}
+
+
+resource "aws_security_group_rule" "sk_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sk.id
+}
+
+
+resource "aws_security_group_rule" "sk_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sk.id
 }
